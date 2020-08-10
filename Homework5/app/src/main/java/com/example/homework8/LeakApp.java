099f0ecf67ddb0889576652c2ffae8978eacd405
butnlit <buntlit@mail.ru>
2020-08-10 18:51:30 +0300

    homework 8, task 1

---
+++ Homework5/app/src/main/java/com/example/homework8/LeakApp.java
@@ -0,0 +1,16 @@
+package com.example.homework8;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+public class LeakApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if(LeakCanary.isInAnalyzerProcess(this)){
+            return;
+        }
+        LeakCanary.install(this);
+    }
+}
